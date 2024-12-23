28d4fd510cca1cd5a782d9e323417891e7d1c600
ankdanil <ankdanil2003@gmail.com>
2024-12-24 01:56:10 +0700

    db

--- lab6/app/build.gradle.kts
+++ lab6/app/build.gradle.kts
@@ -1,11 +1,19 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    kotlin("kapt")
+
 }
+
+
 
 android {
     namespace = "com.example.lab6"
     compileSdk = 35
+
+    buildFeatures {
+        viewBinding = true
+    }
 
     defaultConfig {
         applicationId = "com.example.lab6"
@@ -33,9 +41,17 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+
 }
 
 dependencies {
+
+    implementation(libs.androidx.room.runtime)
+    kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
@@ -45,4 +61,15 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //val roomVersion = "2.5.0" // Проверьте наличие последней версии
+    //implementation("androidx.room:room-runtime:$roomVersion") // Основная библиотека Room
+    //kapt("androidx.room:room-compiler:$roomVersion") // Компилятор аннотаций
+    //implementation("androidx.room:room-ktx:$roomVersion") // Расширения для Kotlin
+
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+//    implementation("androidx.room:room-runtime:2.5.0")
+//    kapt("androidx.room:room-compiler:2.5.0")
+//    implementation("androidx.room:room-ktx:2.5.0")
+
 }
---
+++ lab6/app/src/main/java/com/example/lab6/Violation.kt
@@ -0,0 +1,14 @@
+package com.example.lab6
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(tableName = "violations")
+data class Violation(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    var title: String,
+    var date: String,
+    var isResolved: Boolean
+) : Serializable
+
