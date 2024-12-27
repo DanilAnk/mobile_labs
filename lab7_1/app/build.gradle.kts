1b9dc0a321c09fbcb7bbce84766150b3f611cb33
ankdanil <ankdanil2003@gmail.com>
2024-12-28 01:03:30 +0700

    flicker1

--- lab7_1/app/build.gradle.kts
+++ lab7_1/app/build.gradle.kts
@@ -1,16 +1,17 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
     namespace = "com.example.lab7_1"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.lab7_1"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -37,11 +38,29 @@ android {
 
 dependencies {
 
+    implementation(libs.picasso)
+    implementation(libs.androidx.work.runtime.ktx)
+    implementation(libs.androidx.lifecycle.extensions)
+    implementation(libs.androidx.lifecycle.livedata.ktx.v286)
+    implementation(libs.androidx.preference.ktx)
+
+    implementation("androidx.room:room-runtime:2.6.1") // Основная библиотека Room
+    kapt("androidx.room:room-compiler:2.6.1") // Компилятор для аннотаций
+    implementation("androidx.room:room-ktx:2.6.1") // KTX для удобства работы с Room
+
+
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson.v290)
+    implementation(libs.gson)
+    implementation(libs.converter.scalars)
+    implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.fragment.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
