36c8c239f30d2883aa7392175cb6a1ae2010ac00
ankdanil <ankdanil2003@gmail.com>
2025-01-08 19:18:55 +0700

    room

--- lab8/app/build.gradle.kts
+++ lab8/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -40,9 +41,14 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.room:room-runtime:2.6.1") // Основная библиотека Room
+    kapt("androidx.room:room-compiler:2.6.1") // Компилятор для аннотаций
+    implementation("androidx.room:room-ktx:2.6.1") // KTX для удобства работы с Room
 }
