85c0b2b8ba44ab1848dcc35f2ef4148b4727203b
ankdanil <ankdanil2003@gmail.com>
2025-01-08 19:06:27 +0700

    model and db

---
+++ lab8/app/src/main/java/com/example/lab8/db/TaskDao.kt
@@ -0,0 +1,20 @@
+package com.example.lab8.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Delete
+import com.example.lab8.model.Task
+
+@Dao
+interface TaskDao {
+
+    @Insert
+    suspend fun insert(task: Task)
+
+    @Query("SELECT * FROM tasks")
+    suspend fun getAllTasks(): MutableList<Task>
+
+    @Delete
+    suspend fun delete(task: Task)
+}
